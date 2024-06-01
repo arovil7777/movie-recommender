93a4cb0e896b49a4c9bc2b666715f9f01bdcf5d5
arovil7777 <arovil7777@naver.com>
2024-06-01 20:04:22 +0900

    데이터베이스 설정 초기 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { MoviesModule } from './movies/movies.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { Movie } from './movies/entities/movie.entity';
+import { Review } from './reviews/entities/review.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: '',
+      password: '',
+      database: '',
+      entities: [User, Movie, Review],
+      synchronize: true,
+    }),
+    AuthModule,
+    MoviesModule,
+    ReviewsModule,
+    UsersModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
