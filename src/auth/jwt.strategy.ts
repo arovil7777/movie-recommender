36331c6e0ea9301c3a5c4a92887f2138919c1b88
arovil7777 <arovil7777@naver.com>
2024-06-05 20:13:50 +0900

    jwt strategy 기능 추가 및 eslintrc 수정

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,1 +1,18 @@
-export class JwtStrategy {}
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: '',
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username };
+  }
+}
