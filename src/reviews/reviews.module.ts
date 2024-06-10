a7a8b050c4b3b20b6264555428fca0d6acf7b8bd
arovil7777 <arovil7777@naver.com>
2024-06-10 23:41:45 +0900

    reviews module 기능 추가

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,5 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReviewsService } from './reviews.service';
+import { ReviewsController } from './reviews.controller';
+import { Review } from './entities/review.entity';
 
-@Module({});
-
-export class ReviewsModule { };
+@Module({
+  imports: [TypeOrmModule.forFeature([Review])],
+  controllers: [ReviewsController],
+  providers: [ReviewsService],
+})
+export class ReviewsModule {}
