2d5e73de8c6434ea86e29fe510f3ae7d5b08c784
arovil7777 <arovil7777@naver.com>
2024-06-11 17:31:14 +0900

    reviews service 기능 추가

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,1 +1,20 @@
-export class ReviewsService { }
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Review } from './entities/review.entity';
+
+@Injectable()
+export class ReviewsService {
+  constructor(
+    @InjectRepository(Review)
+    private reviewsRepository: Repository<Review>,
+  ) {}
+
+  create(review: Review): Promise<Review> {
+    return this.reviewsRepository.save(review);
+  }
+
+  findAll(): Promise<Review[]> {
+    return this.reviewsRepository.find();
+  }
+}
