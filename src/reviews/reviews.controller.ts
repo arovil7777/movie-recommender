76ee37ead61e4407cfa68c6dd51d515fe238c599
arovil7777 <arovil7777@naver.com>
2024-06-12 22:37:11 +0900

    reviews controller 기능 추가

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,1 +1,21 @@
-export class ReviewsController { }
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { ReviewsService } from './reviews.service';
+import { Review } from './entities/review.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+@Controller('reviews')
+export class ReviewsController {
+  constructor(private readonly reviewsService: ReviewsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  findAll() {
+    return this.reviewsService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@Body() review: Review) {
+    return this.reviewsService.create(review);
+  }
+}
