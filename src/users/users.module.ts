9cb234d2e4d589376509b17ed6999e7d023e7ea9
arovil7777 <arovil7777@naver.com>
2024-06-15 23:01:39 +0900

    users module 기능 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,13 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
 
-@Module({});
-
-export class UsersModule { };
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
