f600993316366504be0ba7ede5e5795a547ad0e4
arovil7777 <arovil7777@naver.com>
2024-06-16 20:40:14 +0900

    users service 기능 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,20 @@
-export class UsersService { }
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(user: User): Promise<User> {
+    return this.usersRepository.save(user);
+  }
+
+  findOne(username: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+}
