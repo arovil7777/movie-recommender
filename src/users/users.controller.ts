a021212adff5805787e8ee0d18ddc0742bdefd99
arovil7777 <arovil7777@naver.com>
2024-06-17 21:50:12 +0900

    users controller 기능 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,1 +1,13 @@
-export class UsersController {}
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  create(@Body() user: User) {
+    return this.usersService.create(user);
+  }
+}
