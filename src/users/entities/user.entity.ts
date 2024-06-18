2993e75bde1d778454025ba58bc628d0d9000e3d
arovil7777 <arovil7777@naver.com>
2024-06-18 20:55:41 +0900

    users entity 기능 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,17 @@
-import { Entity } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Review } from 'src/reviews/entities/review.entity';
 
 @Entity()
-export class User { }
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Review, review => review.user)
+  reviews: Review[];
+}
